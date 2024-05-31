9d6aff805ea5ced78eff4d862c5ffe9948723c52
Heike Hofmann <hofmann@iastate.edu>
2024-05-31 16:22:02 -0500

    grooves in second row of labelling

--- app/helper.R
+++ app/helper.R
@@ -36,8 +36,7 @@ identify_bullet <- function(words) {
 
 
 groove_plot <- function(ccdata, grooves) {
-#  browser()
-  ccdata %>% 
+    ccdata %>% 
     ggplot(aes(x = x, y = value)) + 
 #    theme_bw()+
     geom_vline(xintercept = 0, colour = "grey50") + 
@@ -47,13 +46,10 @@ groove_plot <- function(ccdata, grooves) {
     annotate("rect", fill="grey50", alpha = 0.15, xmax = Inf, xmin = grooves[2]-grooves[1], ymin = -Inf, ymax = Inf) +
     geom_line(linewidth = 1, data = filter(ccdata, between(x, 0, grooves[2]-grooves[1]))) + # put signal in front
     scale_x_continuous(
-      breaks=c(0,round(as.numeric(grooves[2]-grooves[1]),0),round(seq(min(ccdata$x),max(ccdata$x),by=500),-2))) +
+      breaks=c(0,round(as.numeric(grooves[2]-grooves[1]),0),round(seq(min(ccdata$x),max(ccdata$x),by=500),-2)),
+      labels=c("\n0",paste0("\n",round(as.numeric(grooves[2]-grooves[1]),0)),round(seq(min(ccdata$x),max(ccdata$x),by=500),-2))
+    ) +
     xlab("Position along width of Land [µm]") +
     ylab("Surface Height [µm]") 
-    # theme(
-    #   axis.text=element_text(size=16),
-    #   axis.title=element_text(size=18),
-    #   plot.title = element_text(size=22,face="bold")
-    #   #axis.text.x = element_text(angle = 90, hjust = 1)
-    # ) 
 }
+
