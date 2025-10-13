7f2cf0aa9ff56b0601ff4b30fde084be985721a8
Stephanie Reinders <reinders.stephanie@gmail.com>
2025-10-13 16:28:08 -0500

    chore: add simple script to view land in RGL window

---
+++ docs/developers/view_land.R
@@ -0,0 +1,17 @@
+library(dplyr)
+library(bulletxtrctr)
+library(x3ptools)
+library(rgl)
+library(randomForest)
+library(ggplot2)
+library(readr)
+library(nbtrd)
+
+
+cts <- read_bullet("/Users/stephanie/Documents/cts_set/scans/test_19-527_sample_F2/Item 1/Bullet A")
+image_x3p(cts$x3p[[1]])
+snapshot3d(filename ="~/Documents/CTS19.1.A.png")
+
+hamby <- read_bullet("/Users/stephanie/Documents/hamby44_high_res/Barrel 1/Bullet 1")
+image_x3p(hamby$x3p[[2]])
+snapshot3d(filename ="~/Documents/hamby44.1.2.png")
