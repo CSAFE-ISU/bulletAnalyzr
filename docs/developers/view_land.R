65cf6050e63b0f6b35f219f9c68f8c27a68c24bc
Stephanie Reinders <reinders.stephanie@gmail.com>
2025-11-11 10:11:25 -0600

    chore: add developer helper function to convert dat to x3p

--- docs/developers/view_land.R
+++ docs/developers/view_land.R
@@ -7,32 +7,34 @@ library(ggplot2)
 library(readr)
 library(nbtrd)
 
+load_from_dat <- function(main_dir) {
+  files <- list.files(main_dir, pattern = ".dat", full.names = TRUE)
+  x3ps <- lapply(files, function(f) {
+    df <- readr::read_delim(
+      f,
+      delim = " ",
+      col_names= c("x", "y", "value"),
+      na = c("", "NA", "1.#QNAN0"),
+      progress = FALSE
+    ) # There are no actual Nas created – probably because of the # in the name
+    
+    # Should create a considerable number of NAs
+    df$value <- as.numeric(df$value)
+    
+    # Convert data frame into x3p. Value is measured in microns
+    x3p <- x3ptools::df_to_x3p(df)
+  })
+  df <- data.frame(source = files)
+  df$x3p <- x3ps
+  return(df)
+}
 
-# cts <- read_bullet("/Users/stephanie/Documents/cts_set/scans/test_19-527_sample_F2/Item 1/Bullet A")
-# image_x3p(cts$x3p[[1]])
-# snapshot3d(filename ="~/Documents/CTS19.1.A.png")
-# 
-# hamby <- read_bullet("/Users/stephanie/Documents/Hamby Set 44 Rescan Preprocessed/Barrel 1/Bullet 1")
-# image_x3p(hamby$x3p[[1]])
-# snapshot3d(filename ="~/Documents/hamby44.1.2.png")
+main_dir <- "/Users/stephanie/Downloads/phoenix_example"
 
-main_dir <- "/Users/stephanie/Documents/Bullet_scans/St Louis"
+# Load dat and convert to x3p to view
+b1 <- load_from_dat(main_dir)
+x3p_image(b1$x3p[[1]])
 
-files <- list.files(main_dir, pattern = ".dat", full.names = TRUE)
-df <- readr::read_delim(
-  files[2],
-  delim = " ",
-  col_names= c("x", "y", "value"),
-  na = c("", "NA", "1.#QNAN0"),
-  progress = FALSE
-) # There are no actual Nas created – probably because of the # in the name
-
-# Should create a considerable number of NAs
-df$value <- as.numeric(df$value)
-
-# Convert data frame into x3p. Value is measured in microns
-x3p <- x3ptools::df_to_x3p(df)
-x3p_image(x3p)
-
-# b1 <- read_bullet(main_dir)
-# x3p_image(b1$x3p[[1]])
+# Load from x3p to view
+b1 <- read_bullet(main_dir)
+x3p_image(b1$x3p[[1]])
